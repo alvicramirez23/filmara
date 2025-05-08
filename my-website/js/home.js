ff82dd9bc15d02655a3cf7a0fef031db471a2513
alvicramirez23 <alvicramirez23@gmail.com>
2025-05-08 19:40:55 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'fa94b30f86054ffad37c6d4452edf2fd';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
